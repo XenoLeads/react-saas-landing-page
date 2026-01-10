88f11b958b8723e56f5dfae3bfdda2864f2679cb
Xeno <226741508+XenoLeads@users.noreply.github.com>
2026-01-10 16:38:59 +0530

    code: CTA section

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,5 +1,25 @@
+import { ArrowRight } from "lucide-react";
+
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <div className="w-full bg-indigo-600">
+      <div className="max-w-7xl mx-auto py-20 flex flex-col items-center gap-8">
+        <div className="text-white flex flex-col justify-start items-center gap-8">
+          <h2 className="text-5xl font-bold">Ready to transform your workflow?</h2>
+          <h3 className="text-indigo-100 w-95/100 text-xl text-center">
+            Join thousands of teams who have already made the switch to TaskFlow. Start your free trial today.
+          </h3>
+        </div>
+        <div className="flex justify-center items-center gap-4">
+          <button className="bg-white text-indigo-500 px-8 py-4 rounded-lg flex gap-4 cursor-pointer font-semibold">
+            Start Free Trial <ArrowRight />
+          </button>
+          <button className="px-8 py-4 border-2 rounded-lg border-white text-white cursor-pointer font-semibold">Schedule a Demo</button>
+        </div>
+        <p className="text-indigo-200">14-day free trial • No credit card required • Cancel anytime</p>
+      </div>
+    </div>
+  );
 };
 
 export default CTA;
