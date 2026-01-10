61b9e39607404f7dd31a9dd492e9fbebd8c42606
Xeno <226741508+XenoLeads@users.noreply.github.com>
2026-01-10 18:49:40 +0530

    feat: add mobile navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import logo from "../assets/logo.png";
+import { Menu, X } from "lucide-react";
 
 const Header = ({ scroll_to }) => {
+  const [isToggledNavbar, setIsToggledNavbar] = useState(false);
   return (
-    <header className="h-16 border-b border-neutral-200 w-full fixed top-0 bg-white/50 backdrop-blur-sm z-10">
+    <header className="h-16 border-b border-neutral-200 w-full fixed top-0 bg-white/50 backdrop-blur-sm z-10 overflow-y-visible">
       <div className="max-w-7xl mx-auto h-full flex justify-between items-center px-8">
         <div className="flex justify-start items-center gap-4 cursor-pointer" onClick={scroll_to} data-scroll-to="hero">
           <img src={logo} alt="Logo" className="h-10 rounded-xl" />
           <h1 className="font-semibold text-2xl">XENO</h1>
         </div>
         <nav className="flex gap-12 items-center">
-          <ul className="flex gap-10 text-neutral-700">
+          <ul className="flex gap-10 text-neutral-700 max-md:hidden">
             <li>
               <a className="cursor-pointer" onClick={scroll_to} data-scroll-to="features">
                 Features
@@ -26,9 +29,42 @@ const Header = ({ scroll_to }) => {
               </a>
             </li>
           </ul>
-          <div className="flex gap-8">
+          <div className="flex gap-8 max-md:hidden">
             <button className="text-indigo-500 cursor-pointer">Sign In</button>
             <button className="bg-indigo-500 text-white px-4 py-2 rounded-md cursor-pointer">Start Free Trial</button>
+          </div>
+          <button className="hidden max-md:inline-block" onClick={() => setIsToggledNavbar(prev => !prev)}>
+            {isToggledNavbar ? <X /> : <Menu />}
+          </button>
+
+          <div
+            className={`md:hidden fixed left-0 top-0 mt-16 h-auto w-full flex justify-center items-center bg-neutral-100 text-center py-8 ${
+              isToggledNavbar ? "pointer-events-auto translate-y-0 opacity-100" : "pointer-events-none -translate-y-8 opacity-0"
+            }`}
+          >
+            <div className="w-fit flex flex-col gap-6 ">
+              <ul className="flex flex-col gap-6   text-neutral-700 text-nowrap">
+                <li>
+                  <a className="cursor-pointer" onClick={scroll_to} data-scroll-to="features">
+                    Features
+                  </a>
+                </li>
+                <li>
+                  <a className="cursor-pointer" onClick={scroll_to} data-scroll-to="how-it-works">
+                    How it Works
+                  </a>
+                </li>
+                <li>
+                  <a className="cursor-pointer" onClick={scroll_to} data-scroll-to="about">
+                    About
+                  </a>
+                </li>
+              </ul>
+              <div className="flex flex-col gap-4 text-nowrap">
+                <button className="text-indigo-500 cursor-pointer">Sign In</button>
+                <button className="bg-indigo-500 text-white px-4 py-2 rounded-md cursor-pointer">Start Free Trial</button>
+              </div>
+            </div>
           </div>
         </nav>
       </div>
