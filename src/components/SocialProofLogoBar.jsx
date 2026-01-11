1547b67f3d860c7163233181a46f495b5fb276c6
Xeno <226741508+XenoLeads@users.noreply.github.com>
2026-01-11 15:20:13 +0530

    feat: add inifinite scroll animation on social proof logo bar

--- src/components/SocialProofLogoBar.jsx
+++ src/components/SocialProofLogoBar.jsx
@@ -1,14 +1,25 @@
+const alphabets = "abcdefghijklmnopqrstuvwxyz".split("");
+
 const SocialProofLogoBar = () => {
   return (
     <div className="w-full h-50 bg-neutral-100 border-t border-b border-neutral-200">
       <div className="h-full max-w-7xl mx-auto flex flex-col justify-center items-stretch gap-8 px-8">
         <p className="text-center text-neutral-600 text-sm">Trusted by leading companies worldwide</p>
-        <div className="flex justify-around items-center text-2xl font-semibold text-neutral-300">
-          <p>Company A</p>
-          <p>Company B</p>
-          <p>Company C</p>
-          <p>Company D</p>
-          <p>Company E</p>
+        <div className="group flex overflow-x-clip [&::-webkit-scrollbar]:hidden">
+          <div className="flex justify-start items-center text-2xl font-semibold text-neutral-400 gap-8 animate-infinite-scroll pr-8 group-hover:[animation-play-state:paused]">
+            {alphabets.map(alphabet => (
+              <p key={`1-${alphabet}`} className="text-nowrap shrink-0 grow-0">
+                Company {alphabet.toUpperCase()}
+              </p>
+            ))}
+          </div>
+          <div className="flex justify-start items-center text-2xl font-semibold text-neutral-400 gap-8 animate-infinite-scroll pr-8 group-hover:[animation-play-state:paused]">
+            {alphabets.map(alphabet => (
+              <p key={`2-${alphabet}`} className="text-nowrap shrink-0 grow-0">
+                Company {alphabet.toUpperCase()}
+              </p>
+            ))}
+          </div>
         </div>
       </div>
     </div>
